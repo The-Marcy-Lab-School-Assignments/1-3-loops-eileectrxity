b63c74ae05d4e495da307471136a9aab872e3153
electrxety <er191712@gmail.com>
2024-04-09 01:19:00 -0400

    solved question 3

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -18,9 +18,15 @@ const loop5to10 = () => {
 
 loop5to10(); // 5 6 7 8 9 10
 
-// you do NOT need an if check
+/*Question 3: a for loop that starts on 0, and logs each even number up to, but not including, 10; where 0 counts as an even number for this function
+an if check with continue would technically work here, but it's overkill. a simpler way would be to use i+=2 instead of i++ in the looping condition */
 const loopEvenNumbersUpTo10 = () => {
+  for (let i = 0; i < 10; i+=2) { //with index starting at 0, increment up by 2 to get even numbers stopping when i is no longer less than an exclusive 10
+    console.log(i);
+  };
 };
+
+loopEvenNumbersUpTo10(); //0 2 4 6 8
 
 const countdown5to0 = () => {
 };
