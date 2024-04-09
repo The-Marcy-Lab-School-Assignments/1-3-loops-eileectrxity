653e71b2cc101efe59e006e8968c5751b95089fe
electrxety <er191712@gmail.com>
2024-04-09 01:14:59 -0400

    solved question 2

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -9,8 +9,14 @@ const loop0UpTo10 = () => {
 
 loop0UpTo10(); // 0 1 2 3 4 5 6 7 8 9
 
+//Question 2: a for loop that starts on 5, and logs each number up to, and including, 10; increment each number by 1
 const loop5to10 = () => {
+  for (let i = 5; i <= 10; i++) { //starting at 5, increment indice up by 1 until the stopping condition ending i at an inclusive 10 is met
+    console.log(i);
+  };
 };
+
+loop5to10(); // 5 6 7 8 9 10
 
 // you do NOT need an if check
 const loopEvenNumbersUpTo10 = () => {
