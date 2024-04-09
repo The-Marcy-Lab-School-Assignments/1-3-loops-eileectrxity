2f320f6002d4d3edffaeac9635f9982f925021ce
electrxety <er191712@gmail.com>
2024-04-09 01:12:44 -0400

    solved question 1

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,5 +1,13 @@
+//Lesson 1-3 Assignment: Loops by Eileen
+
+//Question 1: a for loop that starts on 0, and logs each number up to, but not including, 10; increment each number by 1
 const loop0UpTo10 = () => {
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+  };
 };
+
+loop0UpTo10(); // 0 1 2 3 4 5 6 7 8 9
 
 const loop5to10 = () => {
 };
